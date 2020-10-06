ab579d894d7ca4eab98805f368311ecfa24afb79
Fred-S <fredvantage@gmail.com>
2020-10-06 15:51:29 +0200

    add img product items

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -10,8 +10,12 @@ const Header = () => {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='/cart'>Cart</Nav.Link>
-              <Nav.Link href='/login'>Sign In</Nav.Link>
+              <Nav.Link href='/cart'>
+                <i className='fas fa-shopping-cart pr-1'></i>Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i className='fas fa-user pr-1'></i>Sign In
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
